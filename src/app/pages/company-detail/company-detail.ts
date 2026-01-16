320a3be91aeacdc813963c15d0f8cfc1efd16712
Andrea-Mancion <andrea.mancion@epitech.eu>
2026-01-16 17:44:17 +0100

    FEAT(RedirectMap): add a redirectMap when we click on the place

--- src/app/pages/company-detail/company-detail.ts
+++ src/app/pages/company-detail/company-detail.ts
@@ -8,6 +8,7 @@ interface Company {
   roleKey: string;
   periodKey: string;
   locationKey: string;
+  address: string;
   descriptionKey: string;
   missionKey: string;
   achievementsKey: string;
@@ -42,10 +43,15 @@ export class CompanyDetail implements OnInit {
   loadCompany() {
     const companies = this.getAllCompanies();
     this.company = companies.find((c) => c.type === this.companyId) || null;
-    
+
     if (!this.company) {
       this.router.navigate(['/about']);
     }
+  }
+
+  getGoogleMapsUrl(location: string): string {
+    const encodedLocation = encodeURIComponent(location);
+    return `https://www.google.com/maps/search/?api=1&query=${encodedLocation}`;
   }
 
   getAllCompanies(): Company[] {
@@ -55,6 +61,7 @@ export class CompanyDetail implements OnInit {
         roleKey: 'companies.adesio.role',
         periodKey: 'companies.adesio.period',
         locationKey: 'companies.adesio.location',
+        address: '8 Rue du Point du Jour, 35400 Saint-Malo',
         descriptionKey: 'companies.adesio.description',
         missionKey: 'companies.adesio.mission',
         achievementsKey: 'companies.adesio.achievements',
@@ -62,13 +69,14 @@ export class CompanyDetail implements OnInit {
         impactKey: 'companies.adesio.impact',
         image: 'assets/Adesio.png',
         website: 'https://www.adesio.com',
-        type: 'adesio'
+        type: 'adesio',
       },
       {
         nameKey: 'companies.arche.name',
         roleKey: 'companies.arche.role',
         periodKey: 'companies.arche.period',
         locationKey: 'companies.arche.location',
+        address: '1 Rue Pierre et Marie Curie, 22190 Plérin',
         descriptionKey: 'companies.arche.description',
         missionKey: 'companies.arche.mission',
         achievementsKey: 'companies.arche.achievements',
@@ -76,9 +84,8 @@ export class CompanyDetail implements OnInit {
         impactKey: 'companies.arche.impact',
         image: 'assets/ArcheMc2.png',
         website: 'https://www.arche-mc2.com',
-        type: 'arche'
+        type: 'arche',
       },
     ];
   }
 }
-
